98356b97a2e9f53865bf6d2b6da3180b3a026799
Bucuru <47572035+Bucuru@users.noreply.github.com>
2019-04-04 19:22:26 +0200

    Add files via upload

---
+++ ExtraerQLMandos/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmIndicaciones
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bntAbrir = new System.Windows.Forms.Button();
+            this.lblLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bntAbrir
+            // 
+            this.bntAbrir.BackColor = System.Drawing.Color.PaleGreen;
+            this.bntAbrir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntAbrir.Location = new System.Drawing.Point(66, 134);
+            this.bntAbrir.Name = "bntAbrir";
+            this.bntAbrir.Size = new System.Drawing.Size(266, 69);
+            this.bntAbrir.TabIndex = 0;
+            this.bntAbrir.Text = "Elije archivo de mandos para extraer indicaciones";
+            this.bntAbrir.UseVisualStyleBackColor = false;
+            this.bntAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // lblLabel
+            // 
+            this.lblLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLabel.Location = new System.Drawing.Point(27, 35);
+            this.lblLabel.MaximumSize = new System.Drawing.Size(356, 67);
+            this.lblLabel.Name = "lblLabel";
+            this.lblLabel.Size = new System.Drawing.Size(356, 67);
+            this.lblLabel.TabIndex = 1;
+            this.lblLabel.Text = "Esta app extraer todas las _QL de un archivo de mando y las coloca por orden alfa" +
+                "bético en otro archivo";
+            this.lblLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // frmIndicaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 235);
+            this.Controls.Add(this.lblLabel);
+            this.Controls.Add(this.bntAbrir);
+            this.MaximizeBox = false;
+            this.Name = "frmIndicaciones";
+            this.Text = "Extraer indicaciones de mandos";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bntAbrir;
+        private System.Windows.Forms.Label lblLabel;
+    }
+}
+
---
+++ ExtraerQLMandos/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmIndicaciones : Form
+    {
+        List<string> sListaSinRepetidos = new List<string>();
+
+        public frmIndicaciones()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            string sOpenArchivo;
+            
+            OpenFileDialog open = new OpenFileDialog();
+            open.Multiselect = false;
+            open.Filter = "All files|*.sv";
+            open.ShowDialog();
+
+            sOpenArchivo = open.FileName;
+            ExtraerQLs(sOpenArchivo);
+
+            CrearArchivo(sOpenArchivo);                       
+
+        }
+
+        private void ExtraerQLs(string sArchivo)
+        { 
+            string line;
+            List<string> sList =new List<string>();
+                                                
+            try
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader(@sArchivo);
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (line.IndexOf("_QL") != -1 & line.IndexOf("#") == -1)
+                    {
+                        Regex pattern = new Regex(@"\w*_QL", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                        
+                        MatchCollection matches = pattern.Matches(line);
+
+                        foreach(Match m in matches){                            
+                            sList.Add(m.Value);                           
+                        }
+
+                    }
+                }
+                sList.Sort();
+                sListaSinRepetidos = sList.Distinct().ToList();
+            }
+            catch
+            {
+                MessageBox.Show("Error leyendo archivo: " + sArchivo);   
+            }              
+        
+        }
+
+        private void CrearArchivo(string sOpenArchivo)
+        {
+            string sArchivo;
+            int i;
+
+            SaveFileDialog save = new SaveFileDialog();
+            MessageBox.Show("Pulsa Aceptar e introduce el nombre de archivo que se generará.");               
+            save.ShowDialog();
+
+            try
+            {
+                sArchivo = save.FileName;
+                System.IO.StreamWriter file = new System.IO.StreamWriter(@sArchivo);
+
+                
+                //Escribo una cabecera
+                file.WriteLine("# " + DateTime.Now + " -- Archivo generado indicaciones del archivo de mandos: " + sOpenArchivo);
+                
+                //Relleno con la lista de indicaciones de la lista
+                for (i = 0; i < sListaSinRepetidos.Count ; i++)
+                {
+                    if (sListaSinRepetidos[i].IndexOf("_QL") != -1)
+                    {
+                        file.WriteLine(sListaSinRepetidos[i]);
+                    }
+                }
+
+                
+                file.Close();
+                MessageBox.Show("Proceso Completado.");
+
+            }
+            catch
+            {
+                MessageBox.Show("Error creando el archivo.");
+            }
+
+        }
+
+    }
+}
